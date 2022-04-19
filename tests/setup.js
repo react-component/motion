d884c6c584dba0252a6f1adce925a95f9e7913de
zombiej <smith3816@gmail.com>
2022-04-19 10:07:17 +0800

    chore: env prepare

--- tests/setup.js
+++ tests/setup.js
@@ -1,13 +1,9 @@
-const Enzyme = require('enzyme');
-const Adapter = require('enzyme-adapter-react-16');
 const { act } = require('react-dom/test-utils');
 require('regenerator-runtime/runtime');
 
 window.requestAnimationFrame = func => {
   window.setTimeout(func, 16);
 };
-
-Enzyme.configure({ adapter: new Adapter() });
 
 Object.assign(Enzyme.ReactWrapper.prototype, {
   triggerMotionEvent(target) {
