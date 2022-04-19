be511987538307ae1e91a2627a0e60b7a3758941
zombiej <smith3816@gmail.com>
2022-04-19 14:38:06 +0800

    test: list test case

--- tests/setup.js
+++ tests/setup.js
@@ -1,25 +1,5 @@
-const { act } = require('react-dom/test-utils');
 require('regenerator-runtime/runtime');
 
 window.requestAnimationFrame = func => {
   window.setTimeout(func, 16);
 };
-
-// Object.assign(Enzyme.ReactWrapper.prototype, {
-//   triggerMotionEvent(target) {
-//     const motionEvent = new Event('transitionend');
-//     if (target) {
-//       Object.defineProperty(motionEvent, 'target', {
-//         get: () => target.getDOMNode(),
-//       });
-//     }
-
-//     act(() => {
-//       const element = this.find('CSSMotion').getDOMNode();
-//       element.dispatchEvent(motionEvent);
-//       this.update();
-//     });
-
-//     return this;
-//   },
-// });
