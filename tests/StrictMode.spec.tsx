1e8679c8b7df06b35f95066132850f2759c511c6
二货机器人 <smith3816@gmail.com>
2025-02-10 19:23:35 +0800

    feat: motion support ref

--- tests/StrictMode.spec.tsx
+++ tests/StrictMode.spec.tsx
@@ -7,7 +7,7 @@ import classNames from 'classnames';
 import React from 'react';
 import { act } from 'react-dom/test-utils';
 // import type { CSSMotionProps } from '../src/CSSMotion';
-import { genCSSMotion } from '../src/CSSMotion';
+import { genCSSMotion, type CSSMotionRef } from '../src/CSSMotion';
 // import RefCSSMotion, { genCSSMotion } from '../src/CSSMotion';
 // import ReactDOM from 'react-dom';
 
@@ -26,7 +26,7 @@ describe('StrictMode', () => {
   });
 
   it('motion should end', () => {
-    const ref = React.createRef();
+    const ref = React.createRef<CSSMotionRef>();
 
     const { container } = render(
       <React.StrictMode>
@@ -57,6 +57,6 @@ describe('StrictMode', () => {
     fireEvent.transitionEnd(node);
     expect(node).not.toHaveClass('transition-appear');
 
-    expect(ref.current).toBe(node);
+    expect(ref.current.nativeElement).toBe(node);
   });
 });
