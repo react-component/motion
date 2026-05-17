71904c36b199fe1cf5fd5e2027b3a2ed6ba25173
高艳兵 <yanbing.gao@aliyun.com>
2026-05-17 09:20:39 +0800

    chore: remove local deep import lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,6 @@
-const restrictedPackageDirectoryImports = [
-  '@rc-component/*/es',
-  '@rc-component/*/es/**',
-  '@rc-component/*/lib',
-  '@rc-component/*/lib/**',
-  'rc-*/es',
-  'rc-*/es/**',
-  'rc-*/lib',
-  'rc-*/lib/**',
-];
-
 module.exports = {
   extends: [require.resolve('@umijs/fabric/dist/eslint')],
   rules: {
-    'no-restricted-imports': [
-      'error',
-      {
-        patterns: [
-          {
-            group: restrictedPackageDirectoryImports,
-            message:
-              'Do not import package internals from es/lib. Import from the package root.',
-          },
-        ],
-      },
-    ],
     'no-template-curly-in-string': 0,
     'prefer-promise-reject-errors': 0,
     'react/no-array-index-key': 0,
