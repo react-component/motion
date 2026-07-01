0eda310cf660cb3ee97c70fd54b2707326ffd5cb
afc163 <afc163@gmail.com>
2026-07-01 19:13:08 +0800

    chore: remove manual global test declarations

--- global.d.ts
+++ global.d.ts
@@ -8,31 +8,4 @@ declare module '*.css';
 declare module '*.less';
 declare module 'jsonp';
 
-declare namespace JSX {
-  type Element = React.JSX.Element;
-  interface ElementClass extends React.JSX.ElementClass {}
-  interface ElementAttributesProperty
-    extends React.JSX.ElementAttributesProperty {}
-  interface ElementChildrenAttribute
-    extends React.JSX.ElementChildrenAttribute {}
-  type LibraryManagedAttributes<C, P> = React.JSX.LibraryManagedAttributes<
-    C,
-    P
-  >;
-  interface IntrinsicAttributes extends React.JSX.IntrinsicAttributes {}
-  interface IntrinsicClassAttributes<T> extends React.JSX
-    .IntrinsicClassAttributes<T> {}
-  interface IntrinsicElements extends React.JSX.IntrinsicElements {}
-}
-
-declare namespace jest {
-  interface Matchers<R> {
-    lastCalledWith(...expected: unknown[]): R;
-    nthCalledWith(nthCall: number, ...expected: unknown[]): R;
-    toBeCalled(): R;
-    toBeCalledTimes(expected: number): R;
-    toBeCalledWith(...expected: unknown[]): R;
-  }
-}
-
 declare module 'moment/locale/zh-cn';
